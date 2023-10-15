f9af86a2e3ba3c388aedc1bfd224f73d72571b1b
mugiirar <munenevincentmugiira@gmail.com>
2023-10-14 22:18:12 -0700

    review

---
+++ city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""making the city"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    defination of city
+    """
+    state_id = ""
+    name = ""
---
+++ state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""making a stste"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    defination of state
+    """
+    name = ""
